8bda67cefdb8cf09a523a11abf8840e979b3cb66
rorellana-b <ronalbalmoreorellanabonilla@192.168.1.2>
2024-06-09 21:40:03 -0600

    Creating Home Page

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+const dates = document.querySelector("#currentYear");
+const last = document.querySelector("#lastModified");
+
+const today = new Date();
+
+
+dates.innerHTML = `&copy<span>${today.getFullYear()}</span> Ronal Balmore Orellana Bonilla - El Salvador 🇸🇻`
+
+last.innerHTML = `Last Modification:${document.lastModified}`;
